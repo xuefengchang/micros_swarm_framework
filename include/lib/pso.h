b1efbaf6312ecf11aaa6ea4e2d7d279846d3e086
xuefengchang <xuefengchang@outlook.com>
2017-05-03 22:03:27 +0800

    repair bugs

--- include/lib/pso.h
+++ include/lib/pso.h
@@ -69,28 +69,24 @@ namespace micros_swarm_framework{
         public:  
             Particle(float x,float y)
             {
-                //////////////设置粒子属性参数
-                Xmax=30.0f;   //自己通过测试证明，通过限制速度和位置坐标取得了良好的效果，可以很轻易地达到1e-006的精度以上，甚至于能达到1e-009的精度。原因就在于整个群体中的粒子都能更快地集中到适应度高的位置，自然群体最优解就更好。  
+                Xmax=30.0f;
                 Xmin=0.0f;  
                 Ymax=30.0f;  
                 Ymin=0.0f;  
-                Vxmax=Xmax-Xmin;  //通常设置Vmax=Xmax-Xmin;    
+                Vxmax=Xmax-Xmin;
                 Vxmin=0-Vxmax;  
                 Vymax=Ymax-Ymin;  
                 Vymin=0-Vymax;  
   
                 c1=2.0f;  
                 c2=2.0f;
-                //////////////////
     
                 c.x=x;  
                 c.y=y;  
-                //p=100.0f; //先给它一个较大的适应度值（这里我们要得到的是一个较小的适应值）。  
+                //p=100.0f;
                 p=pow(c.x-10.0f,2)+pow(c.y-20.0f,2);  
-                Vx=(Xmax-Xmin)/8.0f;  //////////////////////这里先采用第一种初始化方法，即给所有粒子一个相同的初始速度,为Vmax/8.0f,而Vmax=Xmax-Xmin=30-0=30;注意这个初始速度千万不能太大，自己测试发现它的大小对最终结果的精度影响也很大，有几个数量级。当然，也不能太小，自己测试发现在(Xmax-Xmin)/8.0f时可以得到比较高的精度。  
-                Vy=(Xmax-Xmin)/8.0f;   
-      
-                ///初始时的pBest  
+                Vx=(Xmax-Xmin)/8.0f;
+                Vy=(Xmax-Xmin)/8.0f;
       
                 pBest.x=x;  
                 pBest.y=y;
@@ -112,15 +108,13 @@ namespace micros_swarm_framework{
             {
                 return p;  
             }
-  
-            //void setPBest();  //pBest的设置在setP()中就完成了。  
+
             Coordinate getPBest()const
             {
                 return pBest;  
             }
-      
-            ///////////////////////这是第一种方法，即采用恒定的学习因子。但是实际上可变的学习因子c1,c2可以使种群更快地收敛。此处，将两个维度的速度设置放在同一个函数中。  
-            void setV(Coordinate gBest,float w) //w为惯性因子
+
+            void setV(Coordinate gBest,float w)
             {
                 float r1,r2;  
                 r1=rand()/(float)RAND_MAX;  
@@ -170,36 +164,17 @@ namespace micros_swarm_framework{
             {
                 return c.y;
             }
-  
-            //void outputFile(char Dir[])const;  
         private:  
             Coordinate c;  
-            float p;  //p为适应度。  
-            Coordinate pBest;  
-            ////////////////////二维的话就要有两个速度。  
+            float p;
+            Coordinate pBest;
             float Vx;  
             float Vy;  
             float Xmax,Xmin;  
             float Ymax,Ymin;  
-            float Vxmax,Vxmin; //它们是用来对坐标和速度进行限制的，限制它只能在一定的范围内。  
+            float Vxmax,Vxmin;
             float Vymax,Vymin;  
-            float c1,c2; //c1,c2是学习因子。  
-            //由于要对所有的对象进行比较之后才能得到群体最优，所以它还是不  
-            //static Coordinate gBest; //这个是群体最优.整个群体共享一份就行，所以将它设置成static，但是注意千万不要以为static都是在初始化后就不能修改的，static const才是那样。     
+            float c1,c2;
     };
-  
-    /*void Particle::outputFile(char Dir[])const  
-    {  
-        ofstream out(Dir,ios::app);  //这是添加吧？  
-        out<<this->getX()<<" "<<this->getY()<<" "<<pBest.x<<" "<<pBest.y<<endl;  
-        out.close();  
-    }  
-  
-    ostream& operator<<(ostream &output,const Particle &right)  
-    {  
-        output<<"Now the current coordinates is X:"<<right.getX()<<" Y:"<<right.getY()<<endl;  
-        output<<"And the pBest is X:"<<right.getPBest().x<<" Y:"<<right.getPBest().y<<endl;  
-        return output;  
-    }*/
 };
 #endif
--- lib/testpso.cpp
+++ lib/testpso.cpp
@@ -21,12 +21,9 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSI
 */ 
 #include <stdio.h>
 #include <time.h>  
-#include <string>  
-#include <math.h>  
+#include <string>   
 #include <fstream>  
-#include <iostream> 
-
-#include "ros/ros.h"
+#include <iostream>
 #include "lib/pso.h"
 
 using namespace std;
@@ -35,7 +32,7 @@ using namespace micros_swarm_framework;
 int main()
 {
     Particle*p[40];  
-    float w;//实际上是wmax=1.2f;此处设置wmin=0.5;  
+    float w;
   
     Particle*temp;  
     float randX,randY;  
@@ -51,11 +48,10 @@ int main()
         cout<<"The temp info is X:"<<p[i]->getX()<<" Y:"<<p[i]->getY()<<endl;  
       
     }  
-    //至此，就完成了粒子群的初始化。  
-    //////////////////////////////////////  
-    Coordinate gBest;  //全局最优解。  
+
+    Coordinate gBest;
     int bestIndex=0;  
-    float bestP; //最好的适应度。  
+    float bestP;
     bestP=p[0]->getP();  
     gBest=p[0]->getPBest();  
     for(int i=1;i<40;++i)  
@@ -67,36 +63,22 @@ int main()
             bestIndex=i;  
         }  
     }  
-  
-    ///////////////////////////////////   
+
     cout<<"Now the initial gBest is X:"<<gBest.x<<" Y:"<<gBest.y<<endl;  
     cout<<"And the p[0] is X:"<<p[0]->getX()<<" Y:"<<p[0]->getY()<<endl;  
     cout<<"And the p[39] is X:"<<p[39]->getX()<<" Y:"<<p[39]->getY()<<endl;  
-    cout<<"Now p[0].p="<<p[0]->getP()<<endl;  
-    ////////////////////////////////至此，已经寻找到初始时的种群最优。  
-    /*char buf[20];  
-    for(int i=0;i<40;++i)  
-    {  
-        sprintf(buf,"coordinate%d.dat",i);  
-        ofstream out(buf,ios::out);  
-        out.close();  
-    }*/
-    //////////////////////////这样做是为了每运行一次都重复添加。  
-    for(int k=0;k<100;++k)   //k为迭代次数。  
+    cout<<"Now p[0].p="<<p[0]->getP()<<endl;
+
+    for(int k=0;k<100;++k)
     {
-        w=0.9f-(0.9f-0.4f)*k/99.0f;  //这个因子很重要，既不能太大也不能太小。一开始自己就是设置得太大了导致出错。自己通过计算发现采用可变的惯性因子可以使得到的结果的精确度高一个数量级(达到1e-006)，而如果采用恒定的惯性因子，则只能得到1e-005的精度。  
-        //////////////////////////一开始wmax=1.0,wmin=0.6,可以达到1e-006的精度，以为很高了，但是实际上wmax=0.9f,wmin=0.4f进可以很轻易地达到1e-11的水平，而这已经接近float的精度极限。  
-  
-        // w=0.85f;  
-  
+        w=0.9f-(0.9f-0.4f)*k/99.0f;
+
         for(int i=0;i<40;++i)  
         {  
             temp=p[i];  
             temp->setV(gBest,w);  
             temp->setCoordinate();  
-            temp->setP();  
-            //sprintf(buf,"coordinate%d.dat",i);  
-            //temp->outputFile(buf);    
+            temp->setP();
         }  
         bestP=p[0]->getP();  
         gBest=p[0]->getPBest();  
@@ -108,22 +90,12 @@ int main()
                 bestP=temp->getP();  
                 gBest=temp->getPBest();  
                 bestIndex=i;  
-            }         
-            /* 
-            if((pow(gBest.x-10,2)+pow(gBest.y-20,2))<0.00001f) 
-            { 
-                cout<<"The gBest which is good enough has found!"<<endl; 
-                cout<<"The index is "<<i<<endl; 
-                cout<<"gBest is X:"<<gBest.x<<" Y:"<<gBest.y<<endl; 
-                 exit(0); 
-            } 
-            */  
+            }
         }  
         cout<<"Now gBest is X:"<<gBest.x<<" Y:"<<gBest.y<<" and the minP="<<p[bestIndex]->getP()<<endl;  
         cout<<"bestIndex="<<bestIndex<<endl;  
     }
   
-    getchar();  
-
+    getchar();
     return 0;
 }
